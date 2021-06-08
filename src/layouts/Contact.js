14b701283a1fbf955458847a1b75d3c12fbe30f6
Wiola P <wiol@onet.eu>
2021-06-09 01:19:18 +0200

    contact section style, grid fix & back add

--- src/layouts/Contact.js
+++ src/layouts/Contact.js
@@ -4,131 +4,126 @@ import styled from 'styled-components'
 
 const Contact = () => {
 
-  const logo = `<UV>`
+  const logo = `< UV />`
 
   return (
-    <ContactWrapper>
-      <div className="half-back"></div>
+    <ContactWrapper id="contact">
       <div className="contact-section__wrapper">
-        <article className="contact-section__address">
-          <div className="section-name">
-            <h1>Contact</h1>
-          </div>
-          <div className="address">
-            <p>Wiola Polok</p>
-            <p>Praga, Republika Czeska</p>
-            <p>+ 48 533 484 335</p>
-            <p>+ 420 731 880 053</p>
-            <p>wiola.polok@seznam.cz</p>
-          </div>
-        </article>
-        <article>
-          <form className="contact__form">
-              <label>Name<br/><input type="text" name="name" required></input></label>
-              <label>Email<br/><input type="email" name="mail" required></input></label>
-              <label>Subject<br/><input type="text" name="subject" required></input></label>
-              <label>Message<br/><textarea name="message" rows="6" cols="50" required></textarea></label>
-              <button className="btn contact__btn" type="submit">Send</button>
-          </form>
-          <div className="contact-section__socials">
-            <div><i class="fab fa-facebook-f"></i></div>
-            <div><i class="fab fa-facebook-messenger"></i></div>
-            <div><i class="fab fa-linkedin-in"></i></div>
-            <div><i class="fab fa-github"></i></div>
-            <div><i class="fab fa-codepen"></i></div>
-            <div><i class="fab fa-slack"></i></div>
-            <div><i class="fab fa-discord"></i></div>
-          </div>
-        </article>
-        <p>{logo}</p>
+        <div className="contact-section__section-name">
+          <h1>Contact</h1>
+        </div>
+        <div className="contact-section__address">
+          <p>Wiola Polok</p>
+          <p>Praga, Republika Czeska</p>
+          <p>+ 48 533 484 335</p>
+          <p>+ 420 731 880 053</p>
+          <p>wiola.polok@seznam.cz</p>
+        </div>
+        {/* <form className="contact__form">
+            <label>Name<br/><input type="text" name="name" required></input></label>
+            <label>Email<br/><input type="email" name="mail" required></input></label>
+            <label>Subject<br/><input type="text" name="subject" required></input></label>
+            <label>Message<br/><textarea name="message" rows="6" cols="50" required></textarea></label>
+            <button className="btn contact__btn" type="submit">Send</button>
+        </form> */}
+        <div className="contact-section__socials">
+          <a href="https://www.facebook.com/violinek123"><i class="fab fa-facebook-f"></i></a>
+          <a href="https://www.facebook.com/violinek123"><i class="fab fa-facebook-messenger"></i></a>
+          <a href="https://www.linkedin.com/in/wioleta-polok-44248917/"><i class="fab fa-linkedin-in"></i></a>
+          <a href="https://github.com/Wiolinek"><i class="fab fa-github"></i></a>
+          <a href="https://codepen.io/wiola-p"><i class="fab fa-codepen"></i></a>
+          {/* <a href="https://www.facebook.com/violinek123"><i class="fab fa-slack"></i></a> */}
+          {/* <a href="https://www.facebook.com/violinek123"><i class="fab fa-discord"></i></a> */}
+        </div>
+        <p className="contact-section__logo">{logo}</p>
+        <div className="half-back"></div>
       </div>
+      
     </ContactWrapper>
   );
 }
 
 const ContactWrapper = styled.section`
-  /* background-color: black; */
-  /* height: 50vh; */
   color: white;
-  /* display: flex;
-  flex-direction: column; */
-  /* padding: 10vh 10vw; */
+  padding: 10vh 1vw;
   position: relative;
   .half-back {
       width: 0;
       height: 0;
-      border-top: 90vh solid rgba(0, 0, 0, .75);
-      border-left: 100vw solid transparent;
+      border-top: 120vh solid rgba(0, 0, 0, .75);
+      border-right: 125vw solid transparent;
+      position: absolute;
+      bottom: 0;
+      left: 0;
   }
   .contact-section__wrapper {
-    width: 80%;
-    position: absolute;
-    bottom: 5vh;
-    left: 10vw;
-
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(737px, 740px));
+    grid-template-rows: repeat(auto-fill, minmax(350px, 1fr));
+    grid-auto-rows: 350px;
+    justify-content: center;
+    grid-gap: 2em;
+    margin: 0 auto;
+    z-index: 2;
   }
-    article {
-      /* background-color: green; */
-      display: grid;
-      grid-template-columns: repeat(auto-fill, 737px);
-      grid-auto-rows: 350px;
-      /* align-items: center; */
+    .contact-section__section-name {
+      border: 3px solid purple;
+      display: flex;
       justify-content: center;
-      grid-gap: 2em;
-      /* padding: 10vh 1vw; */
-      .section-name {
-        border: 3px solid purple;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        h1 {
-          font-size: 5em;
-        }
-      }
-      .address {
-        border: 3px solid purple;
-        background-color: purple;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        p {
-          font-size: 1.8em;
-          line-height: 1.5em;
-          letter-spacing: 0.05em;
-          padding: .05em;
-        }
-      }
-      form {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: flex-start;
-        button {
-          width: 30%;
-        }
+      align-items: center;
+      grid-column: 1 / 2;
+      z-index: 2;
+      h1 {
+        font-size: 5em;
       }
     }
-    .contact-section__socials {
-      /* background-color: red; */
+    .contact-section__address {
+      border: 3px solid purple;
+      background-color: purple;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      grid-column: 2 / 3;
+      z-index: 2;
+      p {
+        font-size: 1.8em;
+        line-height: 1.5em;
+        letter-spacing: 0.05em;
+        padding: .05em;
+      }
+    }
+  .contact-section__socials {
+    background-color: purple;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 2;
+    a {
       display: flex;
       align-items: center;
       justify-content: center;
-      div {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border: 2px solid purple;
-        height: 50px;
-        width: 50px;
-        border-radius: 50%;
-        margin: .5em 1.5em;
-        i {
-          color: purple;
-          font-size: 25px;
-        }
+      border: 2px solid black;
+      height: 3.5rem;
+      width: 3.5rem;
+      border-radius: 50%;
+      margin: .5em 1.5em;
+      text-decoration: none;
+      i {
+        color: black;
+        font-size: 1.7rem;
       }
     }
+  }
+  .contact-section__logo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 8rem;
+    color: white;
+    border: 3px solid purple;
+    z-index: 2;
+  }
 `
 
 export default Contact;
