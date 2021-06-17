23b0a1b81afdd26bb7a80b243df96c9781f5a863
Wiola P <wiol@onet.eu>
2021-06-17 23:00:13 +0200

    Projects section style & projects add, photo style

--- src/layouts/Projects.js
+++ src/layouts/Projects.js
@@ -2,73 +2,125 @@ import React from 'react';
 
 import styled from 'styled-components'
 
+import home from '../images/home.png';
+
 const Projects = () => {
 
   return (
     <ProjectsWrapper id="projects">
       <div className="projects-item section-name">
         <h1>Projects</h1>
-        {/* <p>DETAILS</p> */}
       </div>
       <div className="projects-item">
         <div>
-          <div className="projects-item__photo">
-            <p>PORCELANA fotka</p>
-          </div>
+          <p className="projects-item__project-name">Porcelana</p>
+          <div className="projects-item__photo"></div>
           <div className="projects-item__buttons">
             <a href="https://www.facebook.com/violinek123" target="_blank" rel="noreferrer">LIFE</a>
             <a href="https://www.facebook.com/violinek123" target="_blank" rel="noreferrer">CODE</a>
           </div>
         </div>
-        <p>DETAILS</p>
+        <div className="projects-item__details">
+          <ul>
+            <li>React</li>
+            <li>React Router</li>
+            <li>React Scroll</li>
+            <li>GSAP</li>
+            <li>Day.js</li>
+            <li>SASS</li>
+            <li>Axios</li>
+            <li>Express.js</li>
+            <li>MySQL</li>
+          </ul>
+        </div>
       </div>
       <div className="projects-item">
         <div>
-          <div className="projects-item__photo">
-            <p>PHOTO COURSES fotka</p>
-          </div>
+          <p className="projects-item__project-name">Photo Courses</p>
+          <div className="projects-item__photo"></div>
           <div className="projects-item__buttons">
             <a href="https://www.facebook.com/violinek123" target="_blank" rel="noreferrer">LIFE</a>
             <a href="https://www.facebook.com/violinek123" target="_blank" rel="noreferrer">CODE</a>
           </div>
         </div>
-        <p>DETAILS</p>
+        <div className="projects-item__details">
+          <ul>
+            <li>React</li>
+            <li>React Router</li>
+            <li>Redux</li>
+            <li>Redux Persist</li>
+            <li>Google Map React</li>
+            <li>React Image Gallery</li>
+            <li>Day.js</li>
+            <li>BEM</li>
+            <li>Express.js</li>
+            <li>MySQL</li>
+          </ul>
+        </div>
       </div>
       <div className="projects-item">
         <div>
-          <div className="projects-item__photo">
-            <p>PRAGA NIEZWYKŁA fotka</p>
-          </div>
+          <p className="projects-item__project-name">Praga Niezwykła</p>
+          <div className="projects-item__photo"></div>
           <div className="projects-item__buttons">
             <a href="https://www.facebook.com/violinek123" target="_blank" rel="noreferrer">LIFE</a>
             <a href="https://www.facebook.com/violinek123" target="_blank" rel="noreferrer">CODE</a>
           </div>
         </div>
-        <p>DETAILS</p>
+        <div className="projects-item__details">
+          <ul>
+            <li>Wordpress</li>
+            <li>Woocommerce</li>
+            <li>BEM</li>
+          </ul>
+        </div>
       </div>
       <div className="projects-item">
         <div>
-          <div className="projects-item__photo">
-            <p>KOD CEZARA fotka</p>
-          </div>
+          <p className="projects-item__project-name">Cesar Code</p>
+          <div className="projects-item__photo"></div>
           <div className="projects-item__buttons">
             <a href="https://www.facebook.com/violinek123" target="_blank" rel="noreferrer">LIFE</a>
             <a href="https://www.facebook.com/violinek123" target="_blank" rel="noreferrer">CODE</a>
           </div>
         </div>
-        <p>DETAILS</p>
+        <div className="projects-item__details">
+          <ul>
+            <li>Vanilla JS</li>
+          </ul>
+        </div>
       </div>
       <div className="projects-item">
         <div>
-          <div className="projects-item__photo">
-            <p>HOME PAGE fotka</p>
-          </div>
+        <p className="projects-item__project-name">Home Page</p>
+          <div className="projects-item__photo"></div>
           <div className="projects-item__buttons">
             <a href="https://www.facebook.com/violinek123" target="_blank" rel="noreferrer">LIFE</a>
             <a href="https://www.facebook.com/violinek123" target="_blank" rel="noreferrer">CODE</a>
           </div>
         </div>
-        <p>DETAILS</p>
+        <div className="projects-item__details">
+          <ul>
+            <li>React</li>
+            <li>React Styled Components</li>
+            <li>BEM</li>
+          </ul>
+        </div>
+      </div>
+      <div className="projects-item">
+        <div>
+          <p className="projects-item__project-name">Code Snippets</p>
+          <div className="projects-item__photo"></div>
+          <div className="projects-item__buttons">
+            <a href="https://www.facebook.com/violinek123" target="_blank" rel="noreferrer">LIFE</a>
+            <a href="https://www.facebook.com/violinek123" target="_blank" rel="noreferrer">CODE</a>
+          </div>
+        </div>
+        <div className="projects-item__details">
+          <ul>
+            <li>You can also check my work on Codepen</li>
+          </ul>
+        </div>
       </div>
     </ProjectsWrapper>
   );
@@ -101,31 +153,63 @@ const ProjectsWrapper = styled.section`
       border: 3px solid purple;
       grid-column: 1 / 3;
     }
-    p {
+    .projects-item__project-name {
       display: flex;
       align-items: center;
       justify-content: center;
+      min-height: 50px;
+      font-size: 1.5rem;
       border: 3px solid purple;
       background-color: purple;
     }
+    .projects-item__details {
+      /* display: flex; */
+      /* align-items: center; */
+      /* justify-content: center; */
+      border: 3px solid purple;
+      background-color: purple;
+      ul {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        height: 100%;
+        padding: 1em;
+        li {
+          font-size: 1.3rem;
+          letter-spacing: 0.05em;
+          text-align: center;
+        }
+      }
+    }
     .projects-item__photo {
-      height: 300px;
+      height: 250px;
+      background-image: url(${home});
+      background-size: cover;
+      background-position: 20% top;
+      /* img {
+        height: 100%;
+        object-position: center center;
+        object-fit: cover;
+      } */
     }
     .projects-item__buttons {
       display: flex;
       align-items: center;
       justify-content: space-between;
+      /* min-height: 10%; */
       a {
         display: flex;
         align-items: center;
         justify-content: center;
         width: 49%;
         height: 50px;
-        cursor: pointer;
-        text-decoration: none;
+        font-size: 1.5rem;
         color: white;
         text-align: center;
         background-color: purple;
+        text-decoration: none;
+        cursor: pointer;
       }
     }
   }
