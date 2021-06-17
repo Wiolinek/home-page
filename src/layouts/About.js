b8caa3bf42eeeefa9bdefefd6acacbb25660c5ea
Wiola P <wiol@onet.eu>
2021-06-17 22:58:08 +0200

    About section structure & styles change

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -16,95 +16,87 @@ const About = () => {
       <div className="section-name">
         <h1>About Me</h1>
       </div>
-      <div></div>
+      {/* <div></div> */}
       <article>
         <p>Hello, my name is Wiola and I'm a junior front end developer.</p>
         <p>I started my journey with programming in March 2020 and all you can see here is effect of my hard work during last year.</p>
         <p>I based my knowledge and skills on on-line courses and tutorials and official documentation.</p>
         <p>With your little help I am sure I can achieve much, much more ;)</p>
         <p>Interested in cooperation? Just...</p>
-        <button>CONTACT ME!</button>
       </article>
+      <div className="contact-me-btn">
+        <a href="#contact">Contact Me!</a>
+      </div>
     </AboutWrapper>
   );
 }
 
 const AboutWrapper = styled.section`
   background-color: rgba(0, 0, 0, .75);
+  min-height: 100vh;
+  color: white;
+  padding: 10vh 1vw;
   display: grid;
-  grid-template-columns: repeat(2, minmax(200px, .5fr));
-  grid-auto-rows: min-content;
+  grid-template-columns: repeat(auto-fill, 350px);
+  grid-auto-rows: 350px;
   align-items: center;
   justify-content: center;
   grid-gap: 2em;
-  /* width: 100vw; */
-  /* min-height: 100vh; */
-  color: white;
-  padding: 10vh 5vw;
-    .section-name {
-      /* background-color: green; */
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      h1 {
-        font-size: 5em;
-      }
+  margin: 0 auto;
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 3px solid purple;
+    height: 100%;
+  }
+  .section-name {
+    grid-column: 3 / 5;
+    h1 {
+      font-size: 5em;
     }
-    .my-photo {
-      /* background-color: red; */
-      display: flex;
-      /* align-self: flex-start; */
-      justify-content: center;
-      position: relative;
-      /* align-content: center; */
-      /* text-align: center; */
-      /* width: 60%; */
+  }
+  .my-photo {
+    grid-column: 1 / 3;
+    /* align-self: flex-start; */
+    /* position: relative; */
+    /* align-content: center; */
+    /* text-align: center; */
+    /* width: 60%; */
+    img {
+      /* position: relative; */
+      width: 250px;
       /* border: 3px solid purple; */
-      
-      div {
-        /* transform-origin: 50% 50%; */
-        /* transform:  translateX(-50%) translateY(-50%) rotate(-90deg); */
-        /* height: 10vw; */
-        text-align: center;
-        /* vertical-align: middle; */
-        background-color: purple;
-        /* position: absolute; */
-        top: 0;
-        left: 5vw;
-        /* height: 100px; */
-        /* width: 400px; */
-        /* transform: translate(-40%, 100%) rotate(-90deg); */
-        
-      }
-      img {
-        /* position: relative; */
-        width: 350px;
-        /* border: 3px solid purple; */
-        z-index: 10;
-      }
+      z-index: 10;
     }
-    .my-photo::after {
-      content: "";
-      position: absolute;
-      width: 350px;
-      max-height: 100%;
-      top: 10%;
+  }
+    /* .my-photo::after { */
+      /* content: ""; */
+      /* position: absolute; */
+      /* width: 350px; */
+      /* max-height: 100%; */
+      /* top: 10%; */
       /* bottom: -10%; */
       /* left: -10%; */
-      right: 10%;
-      background-color: purple;
-      border: 7px solid purple;
-      z-index: 0;
-    }
+      /* right: 10%; */
+      /* background-color: purple; */
+      /* border: 7px solid purple; */
+      /* z-index: 0; */
+    /* } */
     
   article {
-    grid-column: 1 / 3;
-    padding: 1em 6em;
+    grid-column: 1 / 4;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: .5em;
+    border: 3px solid purple;
+    height: 100%;
     p {
-      font-size: 1.8em;
+      font-size: 1.5rem;
       line-height: 1.5em;
       letter-spacing: 0.05em;
-      padding: .05em;
+      padding: .3em;
       /* font-family: 'Aldrich', sans-serif; */
     /* font-family: 'Quantico', sans-serif; */
     /* font-family: 'Electrolize', sans-serif; */
@@ -114,11 +106,12 @@ const AboutWrapper = styled.section`
     /* font-family: 'Iceland', cursive; */
     /* font-weight: 700; */
     }
-    button {
+  }
+  .contact-me-btn {
+    a {
+      grid-column: 4 / 5;
       font-family: 'Turret Road', cursive;
       font-size: 1.3rem;
-      text-align: center;
-      min-width: 8%;
       padding: .5em 1em;
       margin: 1em;
       border-color: black;
@@ -126,14 +119,15 @@ const AboutWrapper = styled.section`
       color: white;
       border-style: none;
       outline: none;
+      text-decoration: none;
       cursor: pointer;
       transition: .5s;
     }
-    button:hover {
+    a:hover {
       background-color: purple;
       box-shadow: 0 0 20px purple, 0 0 50px purple, 0 0 80px purple, 0 0 110px purple;
     }
-}
+  }
 `
 
 export default About;
